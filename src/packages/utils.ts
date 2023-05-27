28df76ef87b0b1289edd5a5e4c743438a7e0d784
gausszhou <gausszhou@qq.com>
2023-05-27 23:55:08 +0800

    refactor: utils

--- src/packages/utils.ts
+++ src/packages/utils.ts
@@ -1,188 +1,472 @@
+//useful functions
+//from stackoverflow http://stackoverflow.com/questions/1354064/how-to-convert-characters-to-html-entities-using-plain-javascript
+
 import { LiteGUIWidget } from "../types/core";
 
+export function createElementFromString(str: string) {
+  const element = document.createElement("div");
+  element.outerHTML = str;
+  return element;
+}
+
 export function purgeElement(d: LiteGUIWidget) {
-	let a = d.attributes;
-	let i;
-	let l;
-	let n;
+  let attributes = d.attributes;
+  let i;
+  let l;
+  let n;
 
-	if (a) {
-		for (i = a.length - 1; i >= 0; i -= 1) {
-			n = a[i].name;
-			if (typeof d[n] === "function") {
-				d[n] = null;
-			}
-		}
-	}
+  // purge
+  if (attributes) {
+    for (i = attributes.length - 1; i >= 0; i -= 1) {
+      n = attributes[i].name;
+      if (typeof d[n] === "function") {
+        d[n] = null;
+      }
+    }
+  }
 
-	a = d.childNodes;
-	if (a) {
-		l = a.length;
-		for (i = 0; i < l; i += 1) {
-			purgeElement(d.childNodes[i]);
-		}
-	}
+  attributes = d.childNodes;
+  // 递
+  if (attributes) {
+    l = attributes.length;
+    for (i = 0; i < l; i += 1) {
+      purgeElement(d.childNodes[i]);
+    }
+  }
+  // 归
 }
 
 const RESERVED = [
-	"abstract",
-	"else",
-	"instanceof",
-	"super",
-	"boolean",
-	"enum",
-	"int",
-	"switch",
-	"break",
-	"export",
-	"interface",
-	"synchronized",
-	"byte",
-	"extends",
-	"let",
-	"this",
-	"case",
-	"false",
-	"long",
-	"throw",
-	"catch",
-	"final",
-	"native",
-	"throws",
-	"char",
-	"finally",
-	"new",
-	"transient",
-	"class",
-	"float",
-	"null",
-	"true",
-	"const",
-	"for",
-	"package",
-	"try",
-	"continue",
-	"function",
-	"private",
-	"typeof",
-	"debugger",
-	"goto",
-	"protected",
-	"var",
-	"default",
-	"if",
-	"public",
-	"void",
-	"delete",
-	"implements",
-	"return",
-	"volatile",
-	"do",
-	"import",
-	"short",
-	"while",
-	"double",
-	"in",
-	"static",
-	"with"
+  "abstract",
+  "else",
+  "instanceof",
+  "super",
+  "boolean",
+  "enum",
+  "int",
+  "switch",
+  "break",
+  "export",
+  "interface",
+  "synchronized",
+  "byte",
+  "extends",
+  "let",
+  "this",
+  "case",
+  "false",
+  "long",
+  "throw",
+  "catch",
+  "final",
+  "native",
+  "throws",
+  "char",
+  "finally",
+  "new",
+  "transient",
+  "class",
+  "float",
+  "null",
+  "true",
+  "const",
+  "for",
+  "package",
+  "try",
+  "continue",
+  "function",
+  "private",
+  "typeof",
+  "debugger",
+  "goto",
+  "protected",
+  "var",
+  "default",
+  "if",
+  "public",
+  "void",
+  "delete",
+  "implements",
+  "return",
+  "volatile",
+  "do",
+  "import",
+  "short",
+  "while",
+  "double",
+  "in",
+  "static",
+  "with"
 ];
+
 export function beautifyCode(code: string, reserved: string[], skip_css: boolean) {
-	reserved = reserved || RESERVED;
+  reserved = reserved || RESERVED;
 
-	//reserved words
-	code = code.replace(/\b(\w+)\b/g, function (v) {
-		if (reserved.indexOf(v) != -1) return "<span class='rsv'>" + v + "</span>";
-		return v;
-	});
+  //reserved words
+  code = code.replace(/\b(\w+)\b/g, function (v) {
+    if (reserved.indexOf(v) != -1) return "<span class='rsv'>" + v + "</span>";
+    return v;
+  });
 
-	//numbers
-	code = code.replace(/\b([0-9]+)\b/g, function (v) {
-		return "<span class='num'>" + v + "</span>";
-	});
+  //numbers
+  code = code.replace(/\b([0-9]+)\b/g, function (v) {
+    return "<span class='num'>" + v + "</span>";
+  });
 
-	//obj.method
-	code = code.replace(/(\w+\.\w+)/g, function (v) {
-		var t = v.split(".");
-		return "<span class='obj'>" + t[0] + "</span>.<span class='prop'>" + t[1] + "</span>";
-	});
+  //obj.method
+  code = code.replace(/(\w+\.\w+)/g, function (v) {
+    var t = v.split(".");
+    return "<span class='obj'>" + t[0] + "</span>.<span class='prop'>" + t[1] + "</span>";
+  });
 
-	//function
-	code = code.replace(/(\w+)\(/g, function (v) {
-		return "<span class='prop'>" + v.substr(0, v.length - 1) + "</span>(";
-	});
+  //function
+  code = code.replace(/(\w+)\(/g, function (v) {
+    return "<span class='prop'>" + v.substr(0, v.length - 1) + "</span>(";
+  });
 
-	//strings
-	code = code.replace(/(\"(\\.|[^\"])*\")/g, function (v) {
-		return "<span class='str'>" + v + "</span>";
-	});
+  //strings
+  code = code.replace(/(\"(\\.|[^\"])*\")/g, function (v) {
+    return "<span class='str'>" + v + "</span>";
+  });
 
-	//comments
-	code = code.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, function (v) {
-		///(\/\/[a-zA-Z0-9\?\!\(\)_ ]*)/g
-		return "<span class='cmnt'>" + v.replace(/<[^>]*>/g, "") + "</span>";
-	});
+  //comments
+  code = code.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, function (v) {
+    ///(\/\/[a-zA-Z0-9\?\!\(\)_ ]*)/g
+    return "<span class='cmnt'>" + v.replace(/<[^>]*>/g, "") + "</span>";
+  });
 
-	if (!skip_css)
-		code =
-			"<style>.obj { color: #79B; } .prop { color: #B97; }	.str,.num { color: #A79; } .cmnt { color: #798; } .rsv { color: #9AB; } </style>" +
-			code;
-
-	return code;
+  if (!skip_css)
+    code =
+       `<style>
+        .obj { color: #79B; }
+        .prop { color: #B97; }
+        .str,.num { color: #A79; }
+        .cmnt { color: #798; }
+        .rsv { color: #9AB; }
+       </style>
+       ${code}
+       `
+  return code;
 }
 
 export function beautifyJSON(code: string, skip_css: boolean) {
-	if (typeof code == "object") code = JSON.stringify(code);
+  if (typeof code == "object") code = JSON.stringify(code);
 
-	var reserved = ["false", "true", "null"];
+  var reserved = ["false", "true", "null"];
 
-	//reserved words
-	code = code.replace(/(\w+)/g, function (v) {
-		if (reserved.indexOf(v) != -1) return "<span class='rsv'>" + v + "</span>";
-		return v;
-	});
+  //reserved words
+  code = code.replace(/(\w+)/g, function (v) {
+    if (reserved.indexOf(v) != -1) return "<span class='rsv'>" + v + "</span>";
+    return v;
+  });
 
-	//numbers
-	code = code.replace(/([0-9]+)/g, function (v) {
-		return "<span class='num'>" + v + "</span>";
-	});
+  //numbers
+  code = code.replace(/([0-9]+)/g, function (v) {
+    return "<span class='num'>" + v + "</span>";
+  });
 
-	//obj.method
-	code = code.replace(/(\w+\.\w+)/g, function (v) {
-		var t = v.split(".");
-		return "<span class='obj'>" + t[0] + "</span>.<span class='prop'>" + t[1] + "</span>";
-	});
+  //obj.method
+  code = code.replace(/(\w+\.\w+)/g, function (v) {
+    var t = v.split(".");
+    return "<span class='obj'>" + t[0] + "</span>.<span class='prop'>" + t[1] + "</span>";
+  });
 
-	//strings
-	code = code.replace(/(\"(\\.|[^\"])*\")/g, function (v) {
-		return "<span class='str'>" + v + "</span>";
-	});
+  //strings
+  code = code.replace(/(\"(\\.|[^\"])*\")/g, function (v) {
+    return "<span class='str'>" + v + "</span>";
+  });
 
-	//comments
-	code = code.replace(/(\/\/[a-zA-Z0-9\?\!\(\)_ ]*)/g, function (v) {
-		return "<span class='cmnt'>" + v + "</span>";
-	});
+  //comments
+  code = code.replace(/(\/\/[a-zA-Z0-9\?\!\(\)_ ]*)/g, function (v) {
+    return "<span class='cmnt'>" + v + "</span>";
+  });
 
-	if (!skip_css)
-		code =
-			"<style>.obj { color: #79B; } .prop { color: #B97; }	.str { color: #A79; } .num { color: #B97; } .cmnt { color: #798; } .rsv { color: #9AB; } </style>" +
-			code;
+  if (!skip_css)
+    code =
+      "<style>.obj { color: #79B; } .prop { color: #B97; }	.str { color: #A79; } .num { color: #B97; } .cmnt { color: #798; } .rsv { color: #9AB; } </style>" +
+      code;
 
-	return code;
+  return code;
 }
 
 export function dataURItoBlob(dataURI: string): Blob {
-	var pos = dataURI.indexOf(",");
-	//convert to binary
-	var byteString = atob(dataURI.substr(pos + 1));
-	//copy from string to array
-	var ab = new ArrayBuffer(byteString.length);
-	var ia = new Uint8Array(ab);
-	var l = byteString.length;
-	for (var i = 0; i < l; i++) {
-		ia[i] = byteString.charCodeAt(i);
-	}
+  var pos = dataURI.indexOf(",");
+  //convert to binary
+  var byteString = atob(dataURI.substr(pos + 1));
+  //copy from string to array
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  var l = byteString.length;
+  for (var i = 0; i < l; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
 
-	var mime = dataURI.substr(5, pos - 5);
-	mime = mime.substr(0, mime.length - 7); //strip ";base64"
-	return new Blob([ab], { type: mime });
+  var mime = dataURI.substr(5, pos - 5);
+  mime = mime.substr(0, mime.length - 7); //strip ";base64"
+  return new Blob([ab], { type: mime });
 }
+
+// all HTML4 entities as defined here: http://www.w3.org/TR/html4/sgml/entities.html
+// added: amp, lt, gt, quot and apos
+const entityTable = {
+    34: "quot",
+    38: "amp",
+    39: "apos",
+    60: "lt",
+    62: "gt",
+    160: "nbsp",
+    161: "iexcl",
+    162: "cent",
+    163: "pound",
+    164: "curren",
+    165: "yen",
+    166: "brvbar",
+    167: "sect",
+    168: "uml",
+    169: "copy",
+    170: "ordf",
+    171: "laquo",
+    172: "not",
+    173: "shy",
+    174: "reg",
+    175: "macr",
+    176: "deg",
+    177: "plusmn",
+    178: "sup2",
+    179: "sup3",
+    180: "acute",
+    181: "micro",
+    182: "para",
+    183: "middot",
+    184: "cedil",
+    185: "sup1",
+    186: "ordm",
+    187: "raquo",
+    188: "frac14",
+    189: "frac12",
+    190: "frac34",
+    191: "iquest",
+    192: "Agrave",
+    193: "Aacute",
+    194: "Acirc",
+    195: "Atilde",
+    196: "Auml",
+    197: "Aring",
+    198: "AElig",
+    199: "Ccedil",
+    200: "Egrave",
+    201: "Eacute",
+    202: "Ecirc",
+    203: "Euml",
+    204: "Igrave",
+    205: "Iacute",
+    206: "Icirc",
+    207: "Iuml",
+    208: "ETH",
+    209: "Ntilde",
+    210: "Ograve",
+    211: "Oacute",
+    212: "Ocirc",
+    213: "Otilde",
+    214: "Ouml",
+    215: "times",
+    216: "Oslash",
+    217: "Ugrave",
+    218: "Uacute",
+    219: "Ucirc",
+    220: "Uuml",
+    221: "Yacute",
+    222: "THORN",
+    223: "szlig",
+    224: "agrave",
+    225: "aacute",
+    226: "acirc",
+    227: "atilde",
+    228: "auml",
+    229: "aring",
+    230: "aelig",
+    231: "ccedil",
+    232: "egrave",
+    233: "eacute",
+    234: "ecirc",
+    235: "euml",
+    236: "igrave",
+    237: "iacute",
+    238: "icirc",
+    239: "iuml",
+    240: "eth",
+    241: "ntilde",
+    242: "ograve",
+    243: "oacute",
+    244: "ocirc",
+    245: "otilde",
+    246: "ouml",
+    247: "divide",
+    248: "oslash",
+    249: "ugrave",
+    250: "uacute",
+    251: "ucirc",
+    252: "uuml",
+    253: "yacute",
+    254: "thorn",
+    255: "yuml",
+    402: "fnof",
+    913: "Alpha",
+    914: "Beta",
+    915: "Gamma",
+    916: "Delta",
+    917: "Epsilon",
+    918: "Zeta",
+    919: "Eta",
+    920: "Theta",
+    921: "Iota",
+    922: "Kappa",
+    923: "Lambda",
+    924: "Mu",
+    925: "Nu",
+    926: "Xi",
+    927: "Omicron",
+    928: "Pi",
+    929: "Rho",
+    931: "Sigma",
+    932: "Tau",
+    933: "Upsilon",
+    934: "Phi",
+    935: "Chi",
+    936: "Psi",
+    937: "Omega",
+    945: "alpha",
+    946: "beta",
+    947: "gamma",
+    948: "delta",
+    949: "epsilon",
+    950: "zeta",
+    951: "eta",
+    952: "theta",
+    953: "iota",
+    954: "kappa",
+    955: "lambda",
+    956: "mu",
+    957: "nu",
+    958: "xi",
+    959: "omicron",
+    960: "pi",
+    961: "rho",
+    962: "sigmaf",
+    963: "sigma",
+    964: "tau",
+    965: "upsilon",
+    966: "phi",
+    967: "chi",
+    968: "psi",
+    969: "omega",
+    977: "thetasym",
+    978: "upsih",
+    982: "piv",
+    8226: "bull",
+    8230: "hellip",
+    8242: "prime",
+    8243: "Prime",
+    8254: "oline",
+    8260: "frasl",
+    8472: "weierp",
+    8465: "image",
+    8476: "real",
+    8482: "trade",
+    8501: "alefsym",
+    8592: "larr",
+    8593: "uarr",
+    8594: "rarr",
+    8595: "darr",
+    8596: "harr",
+    8629: "crarr",
+    8656: "lArr",
+    8657: "uArr",
+    8658: "rArr",
+    8659: "dArr",
+    8660: "hArr",
+    8704: "forall",
+    8706: "part",
+    8707: "exist",
+    8709: "empty",
+    8711: "nabla",
+    8712: "isin",
+    8713: "notin",
+    8715: "ni",
+    8719: "prod",
+    8721: "sum",
+    8722: "minus",
+    8727: "lowast",
+    8730: "radic",
+    8733: "prop",
+    8734: "infin",
+    8736: "ang",
+    8743: "and",
+    8744: "or",
+    8745: "cap",
+    8746: "cup",
+    8747: "int",
+    8756: "there4",
+    8764: "sim",
+    8773: "cong",
+    8776: "asymp",
+    8800: "ne",
+    8801: "equiv",
+    8804: "le",
+    8805: "ge",
+    8834: "sub",
+    8835: "sup",
+    8836: "nsub",
+    8838: "sube",
+    8839: "supe",
+    8853: "oplus",
+    8855: "otimes",
+    8869: "perp",
+    8901: "sdot",
+    8968: "lceil",
+    8969: "rceil",
+    8970: "lfloor",
+    8971: "rfloor",
+    9001: "lang",
+    9002: "rang",
+    9674: "loz",
+    9824: "spades",
+    9827: "clubs",
+    9829: "hearts",
+    9830: "diams",
+    338: "OElig",
+    339: "oelig",
+    352: "Scaron",
+    353: "scaron",
+    376: "Yuml",
+    710: "circ",
+    732: "tilde",
+    8194: "ensp",
+    8195: "emsp",
+    8201: "thinsp",
+    8204: "zwnj",
+    8205: "zwj",
+    8206: "lrm",
+    8207: "rlm",
+    8211: "ndash",
+    8212: "mdash",
+    8216: "lsquo",
+    8217: "rsquo",
+    8218: "sbquo",
+    8220: "ldquo",
+    8221: "rdquo",
+    8222: "bdquo",
+    8224: "dagger",
+    8225: "Dagger",
+    8240: "permil",
+    8249: "lsaquo",
+    8250: "rsaquo",
+    8364: "euro"
+};
+
+export function escapeHtmlEntities  (text:string) {
+  return text.replace(/[\u00A0-\u2666<>\&]/g, function (c) {
+    const charCode = c.charCodeAt(0)
+    return "&" + (entityTable[charCode as keyof typeof entityTable] || "#" +charCode) + ";";
+  });
+};
