e57be8ba47b8d8b0e49e17048d6cb2ec225346dc
gausszhou <gausszhou@qq.com>
2025-05-28 23:57:42 +0800

    refactor: 重构 Widget

--- src/packages/Widget.ts
+++ src/packages/Widget.ts
@@ -19,4 +19,43 @@ export class Widget extends EventMitter {
         }
         return this;
     }
+
+    width(width: number | string): this {
+        if (typeof width === 'number') {
+            this.root.style.width = width + 'px';
+        } else {
+            this.root.style.width = width;
+        }
+        return this;
+    }
+
+    height(height: number | string): this {
+        if (typeof height === 'number') {
+            this.root.style.height = height + 'px';
+        } else {
+            this.root.style.height = height;
+        }
+        return this;
+    }
+
+    /**
+     * 设置元素的 class
+     * @param className class 名    
+     * @returns this 当前实例，支持链式调用
+     */
+    class(className: string): this {
+        this.root.className = className;
+        return this;
+    }
+
+    /**
+     * 设置元素的 id
+     * @param id id 名
+     * @returns this 当前实例，支持链式调用
+     */
+    id(id: string): this {
+        this.root.id = id;
+        return this;
+    }
+    
 }
